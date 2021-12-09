0677c0dbd7915fd8526fe70d8fe8b1ee2cf3156e
Ludéric Freymann <fr.luderic8@gmail.com>
2021-12-09 11:46:18 +0100

    fix server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/tp03-freymann-luderic"));
+app.use(express.static(__dirname + "/dist/tp02-freymann-luderic"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/tp03-freymann-luderic/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/tp02-freymann-luderic/index.html"));
 });
 app.listen(process.env.PORT || 8080);
