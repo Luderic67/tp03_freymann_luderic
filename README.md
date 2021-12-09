8d6f0c96495b9279dc7d56912166c9a2c2e6b80d
Ludéric Freymann <fr.luderic8@gmail.com>
2021-12-09 11:40:37 +0100

    Fix scss + fix heroku build

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tp02FreymannLuderic
+# Tp03FreymannLuderic
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.3.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tp02-freymann-luderic'`, () => {
+  it(`should have as title 'tp03-freymann-luderic'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tp02-freymann-luderic');
+    expect(app.title).toEqual('tp03-freymann-luderic');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tp02-freymann-luderic app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'tp03-freymann-luderic app is running!'
+    );
   });
 });
